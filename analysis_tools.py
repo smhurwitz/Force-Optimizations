c8b50175455efca441c0792930f4ab30fd43dffc
Siena Hurwitz <shurwitz@umd.edu>
2024-06-17 12:53:54 -0400

    added a sanity check function to compare LCFS, QFMs, and Poincare

--- analysis_tools.py
+++ analysis_tools.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import glob
 import json
 import matplotlib.pyplot as plt
@@ -9,18 +7,33 @@ import pandas as pd
 import plotly.express as px
 import scipy
 import shutil
+import simsopt
+from desc.grid import LinearGrid
+from desc.geometry import FourierRZToroidalSurface
+from desc.equilibrium import Equilibrium
+from desc.vmec_utils import ptolemy_identity_fwd
 from matplotlib import colors
 from mayavi import mlab
 from numbers import Number
 from paretoset import paretoset
 from simsopt._core.optimizable import load
-from simsopt.field import BiotSavart
+from simsopt.field import (BiotSavart,
+                           InterpolatedField, 
+                           SurfaceClassifier, 
+                           particles_to_vtk,
+                           compute_fieldlines, 
+                           LevelsetStoppingCriterion)
 from simsopt.field.force import coil_force, self_force
 from simsopt.field.selffield import regularization_circ
-from simsopt.geo import SurfaceRZFourier
+from simsopt.geo import QfmResidual, QfmSurface, SurfaceRZFourier, Volume
+from simsopt.util import comm_world
 
 
-### DATA ANALYSIS #############################################################
+
+###############################################################################
+# I) DATA ANALYSIS
+###############################################################################
+
 def get_dfs(INPUT_DIR='./output/QA/1/optimizations/', OUTPUT_DIR=None):
     """Returns DataFrames for the raw, filtered, and Pareto data."""
     ### STEP 1: Import raw data
@@ -106,7 +119,11 @@ def parameter_correlations(df, sort_by='normalized_BdotN'):
     return df_correlation.sort_values(by=['R'], ascending=False)
 
 
-### PLOTTING ##################################################################
+
+###############################################################################
+# II) PLOTTING
+###############################################################################
+
 def pareto_interactive_plt(df, color='coil_surface_distance'):
     """Creates an interactive plot of the Pareto front."""
     fig = px.scatter(
@@ -293,3 +310,228 @@ def success_plt(df, df_filtered):
 
     plt.tight_layout()
     return fig
+
+
+
+###############################################################################
+# III) PHYSICS STUFF
+###############################################################################
+
+def poincare(UUID, OUT_DIR='./output/QA/1/poincare/', 
+             INPUT_FILE="./inputs/input.LandremanPaul2021_QA", phis=[0.0],
+             nfieldlines=10, tmax_fl=20000, degree=4, R0_min=1.2125346, 
+             R0_max=1.295, interpolate=True, debug=False):
+    """Compute Poincare plots."""
+ 
+    # Directory for output
+    os.makedirs(OUT_DIR, exist_ok=True)
+
+    # Load in the boundary surface:
+    surf = SurfaceRZFourier.from_vmec_input(INPUT_FILE, nphi=200, ntheta=30, range="full torus")
+    nfp = surf.nfp
+
+    # Load in the optimized coils from stage_two_optimization.py:
+    coils_filename = glob.glob(f"./**/{UUID}/biot_savart.json", recursive=True)[0] 
+    bs = simsopt.load(coils_filename)
+
+    sc_fieldline = SurfaceClassifier(surf, h=0.03, p=2)
+    if debug: 
+        surf.to_vtk(OUT_DIR + 'surface')
+        sc_fieldline.to_vtk(OUT_DIR + 'levelset', h=0.02)
+
+    def trace_fieldlines(bfield, label):
+        R0 = np.linspace(R0_min, R0_max, nfieldlines)
+        Z0 = np.zeros(nfieldlines)
+        fieldlines_tys, fieldlines_phi_hits = compute_fieldlines(
+            bfield, R0, Z0, tmax=tmax_fl, tol=1e-16, comm=comm_world,
+            phis=phis, stopping_criteria=[LevelsetStoppingCriterion(sc_fieldline.dist)])
+        if debug: particles_to_vtk(fieldlines_tys, OUT_DIR + f'fieldlines_{label}')
+
+        if len(phis) > 1:
+            raise NotImplementedError("Not yet implemented!")
+            # plot_poincare_data(fieldlines_phi_hits, phis, OUT_DIR + f'poincare_fieldline_{label}.png', dpi=150)
+
+        fig = plt.figure()
+        for j in range(len(fieldlines_phi_hits)):
+            data_this_phi = fieldlines_phi_hits[j][np.where(fieldlines_phi_hits[j][:, 1] == 0)[0], :]
+            if data_this_phi.size == 0:
+                continue
+            r = np.sqrt(data_this_phi[:, 2]**2+data_this_phi[:, 3]**2)
+            plt.scatter(r, data_this_phi[:, 4], marker='o', s=2, linewidths=0)
+
+        return fig
+
+    # Bounds for the interpolated magnetic field chosen so that the surface is
+    # entirely contained in it
+    n = 20
+    rs = np.linalg.norm(surf.gamma()[:, :, 0:2], axis=2)
+    zs = surf.gamma()[:, :, 2]
+    rrange = (np.min(rs), np.max(rs), n)
+    phirange = (0, 2*np.pi/nfp, n*2)
+    zrange = (0, np.max(zs), n//2)
+
+    def skip(rs, phis, zs):
+        rphiz = np.asarray([rs, phis, zs]).T.copy()
+        dists = sc_fieldline.evaluate_rphiz(rphiz)
+        skip = list((dists < -0.05).flatten())
+        return skip
+
+    bsh = InterpolatedField(
+        bs, degree, rrange, phirange, zrange, True, nfp=nfp, stellsym=True, skip=skip
+    )
+    bsh.set_points(surf.gamma().reshape((-1, 3)))
+    bs.set_points(surf.gamma().reshape((-1, 3)))
+    image = trace_fieldlines(bsh, 'bsh') if(interpolate) else trace_fieldlines(bs, 'bs')
+    return image
+
+
+def qfm(UUID, INPUT_FILE="./inputs/input.LandremanPaul2021_QA", vol_frac=0.5):
+    """Generated quadratic flux minimizing surfaces, adapted from
+    https://github.com/hiddenSymmetries/simsopt/blob/master/examples/1_Simple/qfm.py"""
+
+    # We start with an initial guess that is just the stage I LCFS
+    s = SurfaceRZFourier.from_vmec_input(INPUT_FILE, range="full torus", nphi=32, ntheta=32)
+    
+    # Optimize at fixed volume:
+    path = glob.glob("./**/" + UUID + "/biot_savart.json", recursive=True)[0]
+    bs = load(path)
+    qfm = QfmResidual(s, bs)
+    qfm.J()
+    vol = Volume(s)
+    vol_target = vol_frac * vol.J()
+    qfm_surface = QfmSurface(bs, s, vol, vol_target)
+
+    constraint_weight = 1e0  
+    res = qfm_surface.minimize_qfm_penalty_constraints_LBFGS(tol=1e-12, maxiter=1000,
+                                                            constraint_weight=constraint_weight)
+    vol_err = abs((s.volume()-vol_target)/vol_target)
+    residual = np.linalg.norm(qfm.J())
+    # print(f"||vol constraint||={0.5*(s.volume()-vol_target)**2:.8e}, ||residual||={np.linalg.norm(qfm.J()):.8e}")
+    return qfm_surface.surface, vol_err, residual
+
+
+def surf_to_desc(simsopt_surf, LMN=8):
+    """Returns a DESC equilibrium from a simsopt surface, adapted from code 
+    written by Matt Landreman. Note that LMN is a DESC resolution parameter. """
+    nfp = simsopt_surf.nfp
+    ndofs = len(simsopt_surf.x)
+    index = int((ndofs + 1) / 2)
+    xm = simsopt_surf.m[:index]
+    xn = simsopt_surf.n[:index]
+    rmnc = simsopt_surf.x[:index]
+    zmns = np.concatenate(([0], simsopt_surf.x[index:]))
+
+    rmns = np.zeros_like(rmnc)
+    zmnc = np.zeros_like(zmns)
+
+    # Adapted from desc's VMECIO.load around line 126:
+    m, n, Rb_lmn = ptolemy_identity_fwd(xm, xn, s=rmns, c=rmnc)
+    m, n, Zb_lmn = ptolemy_identity_fwd(xm, xn, s=zmns, c=zmnc)
+    surface = np.vstack((np.zeros_like(m), m, n, Rb_lmn, Zb_lmn)).T
+    desc_surface = FourierRZToroidalSurface(
+        surface[:, 3],
+        surface[:, 4],
+        surface[:, 1:3].astype(int),
+        surface[:, 1:3].astype(int),
+        nfp,
+        simsopt_surf.stellsym,
+        check_orientation=False,
+    )
+
+    # To avoid warning message, flip the orientation manually:
+    if desc_surface._compute_orientation() == -1:
+        desc_surface._flip_orientation()
+        assert desc_surface._compute_orientation() == 1
+
+    eq = Equilibrium(
+        surface=desc_surface,
+        L=LMN,
+        M=LMN,
+        N=LMN,
+        L_grid=2 * LMN,
+        M_grid=2 * LMN,
+        N_grid=2 * LMN,
+        sym=True,
+        NFP=nfp,
+        Psi=np.pi * simsopt_surf.minor_radius()**2,
+        ensure_nested=False,
+    )
+
+    # Check that the desc surface matches the input surface.
+    # Grid resolution for testing the surfaces match:
+    ntheta = len(simsopt_surf.quadpoints_theta)
+    nphi = len(simsopt_surf.quadpoints_phi)
+    simsopt_surf2 = SurfaceRZFourier(
+        mpol=simsopt_surf.mpol,
+        ntor=simsopt_surf.ntor,
+        nfp=simsopt_surf.nfp,
+        stellsym=simsopt_surf.stellsym,
+        dofs=simsopt_surf.dofs,
+        quadpoints_phi=np.linspace(0, 1 / simsopt_surf.nfp, nphi, endpoint=False),
+        quadpoints_theta=np.linspace(0, 1, ntheta, endpoint=False),
+    )
+    gamma = simsopt_surf2.gamma()
+
+    grid = LinearGrid(
+        rho=1,
+        theta=np.linspace(0, 2 * np.pi, ntheta, endpoint=False),
+        zeta=np.linspace(0, 2 * np.pi / nfp, nphi, endpoint=False),
+        NFP=nfp,
+    )
+    data = eq.compute(["X", "Y", "Z"], grid=grid)
+
+    def compare_simsopt_desc(simsopt_data, desc_data):
+        desc_arr = desc_data.reshape((ntheta, nphi), order="F")
+        desc_arr = np.vstack((desc_arr[:1, :], np.flipud(desc_arr[1:, :])))
+        np.testing.assert_allclose(simsopt_data, desc_arr, atol=1e-14)
+
+    compare_simsopt_desc(gamma[:, :, 0].T, data["X"])
+    compare_simsopt_desc(gamma[:, :, 1].T, data["Y"])
+    compare_simsopt_desc(gamma[:, :, 2].T, data["Z"])
+
+    # If tests are passed:
+    return eq
+
+
+###############################################################################
+# IV) SANITY CHECKS
+###############################################################################
+
+def check_poincare_and_qfm(UUID="6266c8d4bb25499b899d86e9e3dd2ee2", phi=0.1, 
+              INPUT_FILE="./inputs/input.LandremanPaul2021_QA"):
+    """
+    Checks that the target LCFS, QFM surface, and Poincaré plots all agree.
+    """
+
+    def get_crosssection(s):
+        cs = s.cross_section(phi)
+        r = np.sqrt(cs[:, 0] ** 2 + cs[:, 1] ** 2)
+        r = np.concatenate((r, [r[0]]))
+        z = cs[:, 2]
+        z = np.concatenate((z, [z[0]]))
+        return r, z
+
+    # show poincaré plots
+    poincare(UUID, INPUT_FILE=INPUT_FILE, nfieldlines=10, tmax_fl=20000, 
+        degree=4, phis=[phi])
+    # poincare(UUID, INPUT_FILE=INPUT_FILE, nfieldlines=3, tmax_fl=4000, 
+    #     degree=4, phis=[phi])
+    
+    # show target LCFS
+    LCFS = SurfaceRZFourier.from_vmec_input(INPUT_FILE, range="full torus", nphi=32, ntheta=32)
+    r, z = get_crosssection(LCFS)
+    plt.plot(r, z, linewidth=2, c='k', label="target LCFS")
+
+    # show QFMs at various radial distances
+    N=3
+    for i in range(N):
+        vol_frac = 0.95 - 0.4 * i / (N - 1 + 1e-100)
+        qfm_surf, _, _ = qfm(UUID, INPUT_FILE=INPUT_FILE, vol_frac=vol_frac)
+        r, z = get_crosssection(qfm_surf)
+        if i != N - 1:
+            plt.plot(r, z, '--', linewidth=2, c='k')
+        else:
+            plt.plot(r, z, '--', linewidth=2, c='k', label="QFMs")
+
+    plt.legend()
+    plt.show()
--- test2.py
+++
@@ -1,5 +0,0 @@
-from physics_tools import *
-UUID = "6266c8d4bb25499b899d86e9e3dd2ee2"
-qfm_surf, _, _ = qfm(UUID)
-desc_eq = surf_to_desc(qfm_surf)
-print("tests passed!")
